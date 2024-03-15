ce0bdb8b057bc98b4d6aad92ca9745572bc69ddf
jdefayes <jdefayes@student.42lausanne.ch>
2024-03-15 17:55:01 +0100

    socket handling

---
+++ webserv/include/Client.hpp
@@ -0,0 +1,25 @@
+#ifndef CLIENT_HPP
+#define CLIENT_HPP
+
+#include "ConfigParser.hpp"
+#include "Connection.hpp"
+
+class Client
+{
+    private:
+
+    public:
+        // constructeur et destructeur
+        Client();
+        ~Client();
+        Client(const Client &other);
+        Client &operator=(const Client &other);
+
+
+        // Méthodes///////////////////
+        // GETTERS
+        // SETTERS
+
+};
+
+#endif
--- webserv/include/Server.hpp
+++ webserv/include/Server.hpp
@@ -3,20 +3,32 @@
 
 #include "ConfigParser.hpp"
 #include "Connection.hpp"
+#include <iostream>
+#include <sys/socket.h>
+#include <netinet/in.h>
 
 class Server
 {
     private:
-    
+    int _sockFd;
+    int _newSocket;
+    int _port;
+    int _ipAdress;
+    struct sockaddr_in _addr;
+
     public:
         // constructeur et destructeur
-        Server();
+        Server(int ipAdress, int port);
         ~Server();
+        Server(const Server &other);
+        Server &operator=(const Server &other);
 
+        Init();
+        Run();
         // Méthodes///////////////////
         // GETTERS
         // SETTERS
-    
+
 };
 
 #endif
